79f1586584546834a8b825f03c421381409eeab7
TiesP <pavel.o.71@gmail.com>
2018-02-14 10:41:41 +0300

    замена на switch

--- index.js
+++ index.js
@@ -7,21 +7,28 @@ const license = require(`./src/license`);
 
 const arg = process.argv.slice(2)[0];
 
-if (!arg) {
-  intro.execute();
-} else if (arg === `--version`) {
-  version.execute();
-} else if (arg === `--help`) {
-  help.execute();
-} else if (arg === `--author`) {
-  author.execute();
-} else if (arg === `--description`) {
-  description.execute();
-} else if (arg === `--license`) {
-  license.execute();
-} else {
-  console.error(`Неизвестная команда ${arg}. 
+switch (arg) {
+  case undefined:
+    intro.execute();
+    break;
+  case `--version`:
+    version.execute();
+    break;
+  case `--help`:
+    help.execute();
+    break;
+  case `--author`:
+    author.execute();
+    break;
+  case `--description`:
+    description.execute();
+    break;
+  case `--license`:
+    license.execute();
+    break;
+  default:
+    console.error(`Неизвестная команда ${arg}. 
 Чтобы прочитать правила использования приложения, наберите "--help"`);
-  process.exitCode = 1;
+    process.exitCode = 1;
 }
 
