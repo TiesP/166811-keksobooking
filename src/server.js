614d5be408aea29d25889a5034f24294d59ed054
TiesP <pavel.o.71@gmail.com>
2018-02-26 16:57:06 +0300

    #11 quotes исправлены

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require(`http`);
 const url = require(`url`);
 const fs = require(`fs`);
-const path = require('path');
+const path = require(`path`);
 const {promisify} = require(`util`);
 
 const HOSTNAME = `127.0.0.1`;
