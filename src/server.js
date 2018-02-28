60975430aedf60cb921326ca157201a16a38ed5b
TiesP <pavel.o.71@gmail.com>
2018-02-28 09:04:27 +0300

    #12 express.static вместо старого кода

--- src/server.js
+++ src/server.js
@@ -1,79 +1,18 @@
-const http = require(`http`);
-const url = require(`url`);
-const fs = require(`fs`);
-const path = require(`path`);
-const {promisify} = require(`util`);
+const express = require('express');
 
-const HOSTNAME = `127.0.0.1`;
 const PORT = 3000;
+const app = express();
 
-const readfile = promisify(fs.readFile);
-
-const sendError = (res) => {
-  res.writeHead(404, `Not Found`);
-  res.end(`Not Found`);
-};
-
-const readFileF = async (file, res) => {
-  const data = await readfile(file);
-  const extname = path.extname(file);
-
-  res.statusCode = 200;
-  res.statusMessage = `OK`;
-
-  const types = {
-    '.html': `text/html; charset=UTF-8`,
-    '.ico': `image/x-icon`,
-    '.png': `image/png`,
-    '.css': `text/css`
-  };
-
-  if (types[extname]) {
-    res.setHeader(`Content-Type`, types[extname]);
-  } else {
-    res.setHeader(`Content-Type`, `text/plain`);
-  }
-
-  res.setHeader(`Content-Length`, Buffer.byteLength(data));
-  res.end(data);
-};
-
-
-const sendFile = (file, res) => {
-  const absPath = path.join(__dirname, `..`, `static`, file);
-
-  (async () => {
-    try {
-      await readFileF(absPath, res);
-    } catch (e) {
-      sendError(res);
-    }
-  })().catch((e) => {
-    res.writeHead(500, e.message, {'Content-Type': `text/plain`});
-    res.end(e.message);
-  });
-};
+app.use(express.static('static'));
 
 module.exports = {
   name: `server`,
   description: `Run server`,
   execute(portUser) {
-    const server = http.createServer((req, res) => {
-      const pathname = url.parse(req.url).pathname;
-      if (req.method !== `GET`) {
-        sendError(res);
-      }
-
-      if (pathname === `/`) {
-        sendFile(`/index.html`, res);
-      } else {
-        sendFile(pathname, res);
-      }
-    });
-
     const currentPort = portUser ? portUser : PORT;
-    server.listen(currentPort, HOSTNAME, () => {
-      console.log(`Server running at http://${HOSTNAME}:${currentPort}/`);
-    });
+
+    app.listen(currentPort, () => {
+      console.log(`Server running at http://localhost:${currentPort}/`);
+    })
   }
 };
